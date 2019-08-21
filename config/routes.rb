2995e71bafd93b461f5489598b47689cfa5becc9
Maxim Oberemko <oberemko.maxim@gmail.com>
2019-08-21 15:58:40 +0300

    Added welcome page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "home#index"
   devise_for :admins, path: 'admin', controllers: { sessions: "admins/sessions" }
   devise_for :users, path: 'user', controllers: { sessions: "users/sessions" }
   resources :tasks
