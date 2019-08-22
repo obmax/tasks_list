2160fbdca95381336721e2af42702db5eb32873d
Maxim Oberemko <oberemko.maxim@gmail.com>
2019-08-23 00:40:36 +0300

    Added faker usage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do
+  User.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: '1q2w3e',
+      password_confirmation: '1q2w3e'
+    )  
+end
+
+2.times do
+  Admin.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: '1q2w3e',
+      password_confirmation: '1q2w3e'
+    )  
+end
+
+20.times do
+  Task.create!(
+      subject: Faker::Lorem.word.capitalize,
+      description: Faker::Lorem.paragraph,
+      type: Task.types.sample,
+      status: Task.statuses.values.sample,
+      assignee: User.ids.sample,
+      created_by: Admin.ids.sample
+    )  
+end
